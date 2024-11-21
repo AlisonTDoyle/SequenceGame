a6a62e9105880bc1876929de9de26609f9d53b6f
Alison Doyle <s00233102@atu.ie>
2024-11-21 11:40:26 +0000

    :sparkles: Basic high score screen layout w/ navigation

--- app/src/main/java/com/example/sequencegame/HighScores.java
+++ app/src/main/java/com/example/sequencegame/HighScores.java
@@ -1,6 +1,10 @@
 package com.example.sequencegame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HighScores extends AppCompatActivity {
+    // Properties
+
+    // Activity elements
+    ListView listViewScores;
+    Button buttonReturnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,22 @@ public class HighScores extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Fetch activity elements
+        listViewScores = findViewById(R.id.listViewScores);
+        buttonReturnHome = findViewById(R.id.buttonReturnHome);
+
+        // Event listeners
+        buttonReturnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create new high scores activity
+                Intent mainActivityIntent = new Intent(HighScores.this, MainActivity.class);
+
+                // Display activity
+                startActivity(mainActivityIntent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/sequencegame/MainActivity.java
+++ app/src/main/java/com/example/sequencegame/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.sequencegame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    // Properties
+
+    // Activity elements
+    EditText editTextUserName;
+    Button buttonStartGame;
+    Button buttonViewHighScores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Fetch activity elements
+        editTextUserName = findViewById(R.id.editTextUserName);
+        buttonStartGame = findViewById(R.id.buttonStartGame);
+        buttonViewHighScores = findViewById(R.id.buttonViewHighScores);
+
+        // Event listeners
+        buttonViewHighScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create new high scores activity
+                Intent highScoreActivityIntent = new Intent(MainActivity.this, HighScores.class);
+
+                // Display activity
+                startActivity(highScoreActivityIntent);
+            }
+        });
     }
+
+
 }
